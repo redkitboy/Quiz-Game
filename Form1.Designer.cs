b401a70ee62ebd0dec2b2ebdff79a2a993f1e226
redkitboy <128413256+redkitboy@users.noreply.github.com>
2024-07-19 15:37:05 +0300

    Yanlış hakkı ve puan sayacı eklendi
    ilk yanlışta oyun devam eder, ikinci yanlışta ise ekrana soru gelmez. label3 en sonda görünür olur.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,6 +37,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.dogruTimer = new System.Windows.Forms.Timer(this.components);
             this.yanlisTimer = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -55,7 +57,7 @@
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
             this.button1.Location = new System.Drawing.Point(122, 189);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(209, 53);
+            this.button1.Size = new System.Drawing.Size(242, 59);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = false;
@@ -66,7 +68,7 @@
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
             this.button2.Location = new System.Drawing.Point(435, 189);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(209, 53);
+            this.button2.Size = new System.Drawing.Size(229, 59);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = false;
@@ -77,7 +79,7 @@
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
             this.button3.Location = new System.Drawing.Point(122, 286);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(209, 53);
+            this.button3.Size = new System.Drawing.Size(242, 61);
             this.button3.TabIndex = 3;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = false;
@@ -88,7 +90,7 @@
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
             this.button4.Location = new System.Drawing.Point(435, 286);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(209, 53);
+            this.button4.Size = new System.Drawing.Size(229, 61);
             this.button4.TabIndex = 4;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = false;
@@ -98,11 +100,12 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
             this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label2.Location = new System.Drawing.Point(315, 37);
+            this.label2.Location = new System.Drawing.Point(264, 37);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(70, 26);
             this.label2.TabIndex = 5;
             this.label2.Text = "label2";
+            this.label2.Visible = false;
             // 
             // dogruTimer
             // 
@@ -114,11 +117,39 @@
             this.yanlisTimer.Interval = 200;
             this.yanlisTimer.Tick += new System.EventHandler(this.yanlisTimer_Tick);
             // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.DarkSalmon;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Location = new System.Drawing.Point(626, 39);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(114, 46);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Kapat";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(264, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 26);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -142,6 +173,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Timer dogruTimer;
         private System.Windows.Forms.Timer yanlisTimer;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,46 +15,132 @@ namespace Quiz_Game
     {
         static int sayac = 0;
         static int zamansayac = 0;
+        static int yanlisSayisi = 0;
+        static int puansayac = 0;
         List<Questions> sorularim = new List<Questions>
-            {
-                new Questions
-                {
-                    Qtext = "What is the capital of France?",
-                    Correctanswer = "Paris",
-                    Wronganswers = new string[] { "London", "Berlin", "Rome" }
-                },
-                new Questions
-                {
-                    Qtext = "What is 9 x 3?",
-                    Correctanswer = "27",
-                    Wronganswers = new string[] { "11", "49", "35" }
-                },
-                new Questions
-                {
-                    Qtext = "What is the color of the sky?",
-                    Correctanswer = "Blue",
-                    Wronganswers = new string[] { "Green", "Red", "Yellow" }
-                },
-                new Questions
-                {
-                    Qtext = "What is the capital of Japan?",
-                    Correctanswer = "Tokyo",
-                    Wronganswers = new string[] { "Kyoto", "Osaka", "Hiroshima" }
-                },
-                new Questions
-                {
-                    Qtext = "What is the largest planet in our Solar System?",
-                    Correctanswer = "Jupiter",
-                    Wronganswers = new string[] { "Earth", "Saturn", "Mars" }
-                },
-                new Questions
-                {
-                    Qtext = "Where was Euro 2024 Football Championship held?",
-                    Correctanswer = "Germany",
-                    Wronganswers = new string[] { "Sweden", "Brasil", "Poland" }
-                }
+{
+    new Questions
+    {
+        Qtext = "Türkiye'nin başkenti neresidir?",
+        Correctanswer = "Ankara",
+        Wronganswers = new string[] { "İstanbul", "İzmir", "Bursa" }
+    },
+    new Questions
+    {
+        Qtext = "En büyük okyanus hangisidir?",
+        Correctanswer = "Pasifik Okyanusu",
+        Wronganswers = new string[] { "Atlas Okyanusu", "Hint Okyanusu", "Arktik Okyanusu" }
+    },
+    new Questions
+    {
+        Qtext = "Türkiye'nin en yüksek dağı hangisidir?",
+        Correctanswer = "Ağrı Dağı",
+        Wronganswers = new string[] { "Erciyes Dağı", "Kaçkar Dağı", "Uludağ" }
+    },
+    new Questions
+    {
+        Qtext = "İstanbul Boğazı hangi iki denizi birleştirir?",
+        Correctanswer = "Marmara Denizi ve Karadeniz",
+        Wronganswers = new string[] { "Ege Denizi ve Akdeniz", "Karadeniz ve Ege Denizi", "Marmara Denizi ve Akdeniz" }
+    },
+    new Questions
+    {
+        Qtext = "Türkiye'nin en uzun nehri hangisidir?",
+        Correctanswer = "Kızılırmak",
+        Wronganswers = new string[] { "Fırat Nehri", "Dicle Nehri", "Meriç Nehri" }
+    },
+    new Questions
+    {
+        Qtext = "Dünya'nın uydusu nedir?",
+        Correctanswer = "Ay",
+        Wronganswers = new string[] { "Güneş", "Venüs", "Mars" }
+    },
+    new Questions
+    {
+        Qtext = "Bir yıl kaç gündür?",
+        Correctanswer = "365",
+        Wronganswers = new string[] { "360", "364", "366" }
+    },
+    new Questions
+    {
+        Qtext = "En uzun süre tahtta kalan Osmanlı padişahı kimdir?",
+        Correctanswer = "Kanuni Sultan Süleyman",
+        Wronganswers = new string[] { "Fatih Sultan Mehmet", "Yavuz Sultan Selim", "II. Mahmud" }
+    },
+    new Questions
+    {
+        Qtext = "Rönesans hangi ülkede başlamıştır?",
+        Correctanswer = "İtalya",
+        Wronganswers = new string[] { "Fransa", "İngiltere", "İspanya" }
+    },
+    new Questions
+    {
+        Qtext = "Hangi gezegen halkalarıyla ünlüdür?",
+        Correctanswer = "Satürn",
+        Wronganswers = new string[] { "Mars", "Jüpiter", "Venüs" }
+    },
+    new Questions
+    {
+        Qtext = "Türkiye'nin en büyük gölü hangisidir?",
+        Correctanswer = "Van Gölü",
+        Wronganswers = new string[] { "Tuz Gölü", "Beyşehir Gölü", "Eğirdir Gölü" }
+    },
+    new Questions
+    {
+        Qtext = "Türkiye'nin ilk cumhurbaşkanı kimdir?",
+        Correctanswer = "Mustafa Kemal Atatürk",
+        Wronganswers = new string[] { "İsmet İnönü", "Celal Bayar", "Adnan Menderes" }
+    },
+    new Questions
+    {
+        Qtext = "Dünyanın en yüksek şelalesi hangisidir?",
+        Correctanswer = "Angel Şelalesi",
+        Wronganswers = new string[] { "Niagara Şelalesi", "Victoria Şelalesi", "Iguazu Şelalesi" }
+    },
+    new Questions
+    {
+        Qtext = "Hangi elementin kimyasal sembolü 'O'dur?",
+        Correctanswer = "Oksijen",
+        Wronganswers = new string[] { "Altın", "Gümüş", "Demir" }
+    },
+    new Questions
+    {
+        Qtext = "Dünya'nın en uzun nehri hangisidir?",
+        Correctanswer = "Nil Nehri",
+        Wronganswers = new string[] { "Amazon Nehri", "Yangtze Nehri", "Mississippi Nehri" }
+    },
+    new Questions
+    {
+        Qtext = "Türkçede en çok kullanılan harf hangisidir?",
+        Correctanswer = "A",
+        Wronganswers = new string[] { "E", "I", "O" }
+    },
+    new Questions
+    {
+        Qtext = "Yer çekimini keşfeden bilim insanı kimdir?",
+        Correctanswer = "Isaac Newton",
+        Wronganswers = new string[] { "Albert Einstein", "Galileo Galilei", "Nikola Tesla" }
+    },
+    new Questions
+    {
+        Qtext = "Dünya'nın en büyük çölü hangisidir?",
+        Correctanswer = "Sahra Çölü",
+        Wronganswers = new string[] { "Gobi Çölü", "Arabistan Çölü", "Kalahari Çölü" }
+    },
+    new Questions
+    {
+        Qtext = "Bir futbol maçında kaç oyuncu sahada olur (her iki takım toplamı)?",
+        Correctanswer = "22",
+        Wronganswers = new string[] { "20", "24", "18" }
+    },
+    new Questions
+    {
+        Qtext = "Türk Bayrağı'nın rengi nedir?",
+        Correctanswer = "Kırmızı ve Beyaz",
+        Wronganswers = new string[] { "Mavi ve Beyaz", "Yeşil ve Beyaz", "Kırmızı ve Siyah" }
+    }
+};
 
-            };
         static List<Button> myButtons = new List<Button>();
         static int myRandomNumber;
         static Button wrongButton = new Button();
@@ -67,41 +153,58 @@ namespace Quiz_Game
             myButtons.Add(button3);
             myButtons.Add(button4);
             SoruGetir();
+            button5.Select();
         }
 
         public void SoruGetir()
         {
-            
-            if(sayac<sorularim.Count)
+            if(yanlisSayisi<2)
             {
-                label1.Text = sorularim[sayac].Qtext.ToString();
-                Random random = new Random();
-                myRandomNumber = random.Next(0, 4); // 0 dahil, 4 hariç
-                Console.WriteLine("ŞİMDİ ŞANSLI SAYI: " + myRandomNumber);
-                int yanlisSecnSay = 0;
-                for (int i=0;i<myButtons.Count;i++)
+                Random random1 = new Random();
+                sayac = random1.Next(0, sorularim.Count);
+                if (sayac < sorularim.Count)
                 {
-                    if(i != myRandomNumber)
+                    label1.Text = sorularim[sayac].Qtext.ToString();
+                    Random random = new Random();
+                    myRandomNumber = random.Next(0, 4); // 0 dahil, 4 hariç
+                    Console.WriteLine("ŞİMDİ ŞANSLI SAYI: " + myRandomNumber);
+                    int yanlisSecnSay = 0;
+                    for (int i = 0; i < myButtons.Count; i++)
                     {
-                        myButtons[i].Click += new System.EventHandler(this.Button_Click);
-                        myButtons[i].Text = sorularim[sayac].Wronganswers[yanlisSecnSay];
-                        //Console.WriteLine(i +  "  numarali secenek hatali secenek şeklinde doldurdldu");
-                        yanlisSecnSay++;
+                        if (i != myRandomNumber)
+                        {
+                            myButtons[i].Click += new System.EventHandler(this.Button_Click);
+                            myButtons[i].Text = sorularim[sayac].Wronganswers[yanlisSecnSay];
+                            //Console.WriteLine(i +  "  numarali secenek hatali secenek şeklinde doldurdldu");
+                            yanlisSecnSay++;
+                        }
                     }
-                }
 
-                myButtons[myRandomNumber].Click += new System.EventHandler(this.Dogru_Click);
-                myButtons[myRandomNumber].Text = sorularim[sayac].Correctanswer;
-                Console.WriteLine("BEKLENEN CEVAP: "+myButtons[myRandomNumber].Text);
-                
-                sayac++;
-                Console.WriteLine("\n \n");
-                //Thread.Sleep(1000);
+                    myButtons[myRandomNumber].Click += new System.EventHandler(this.Dogru_Click);
+                    myButtons[myRandomNumber].Text = sorularim[sayac].Correctanswer;
+                    Console.WriteLine("BEKLENEN CEVAP: " + myButtons[myRandomNumber].Text);
+
+                    sayac++;
+                    Console.WriteLine("\n \n");
+                    //Thread.Sleep(1000);
+                }
+                else
+                {
+                    label2.Text = "Bütün soruları cevapladınız";
+                    label2.Visible = true;
+                }
             } else
             {
-                label2.Text = "Bütün soruları cevapladınız";
-                label2.Visible = true;
+                foreach(var item in myButtons)
+                {
+                    item.Visible = false;
+                }
+                label1.Visible = false;
+                label2.Text = "2 Yanlış yaptınız ve oyun bitti.";
+                label2.Visible=true;
+                label3.Visible = true;
             }
+            
 
             
         }
@@ -116,6 +219,8 @@ namespace Quiz_Game
         private void Dogru_Click(object sender, EventArgs e)
         {
             dogruTimer.Start();
+            puansayac++;
+            label3.Text=puansayac.ToString() + " Puan topladınız.";
             Console.WriteLine("DOĞRU CEVAP");
             label2.Text = "doğru cevap !";
             //label2.Visible=true;
@@ -147,14 +252,14 @@ namespace Quiz_Game
                 label2.Visible = true;
                 myButtons[myRandomNumber].BackColor = Color.LawnGreen;
             }
-            else if(zamansayac==10)
+            else if(zamansayac==6)
             {
                 label2.Visible = false;
                 zamansayac = 0;
                 myButtons[myRandomNumber].BackColor= Color.WhiteSmoke;
                 dogruTimer.Stop();
-                //myButtons[myRandomNumber].(); butonun seçili olmasını iptal edeceğiz
                 SoruGetir();
+                button5.Select();
 
             }
         }
@@ -163,9 +268,13 @@ namespace Quiz_Game
 
         public void yanlisCevapFonk()
         {
+            yanlisSayisi++;
             label2.Text = "Yanlış Cevap!";
             yanlisTimer.Start();
-            sayac = 0;
+            if(yanlisSayisi>1)
+            {
+                sayac = 0;
+            }
             for (int i = 0; i < myButtons.Count; i++)
             {
                 if (i != myRandomNumber)
@@ -179,15 +288,14 @@ namespace Quiz_Game
         }
         private void yanlisTimer_Tick(object sender, EventArgs e)
         {
-            //Button wrongbutton = sender as Button;
-
             zamansayac++;
-            if (zamansayac == 2)
+            if (zamansayac == 3)
             {
+                label2.Visible=true;
                 wrongButton.BackColor = Color.Red;
                 myButtons[myRandomNumber].BackColor = Color.LawnGreen;
             }
-            else if (zamansayac == 10)
+            else if (zamansayac == 15)
             {
                 label2.Visible = false;
                 zamansayac = 0;
@@ -195,7 +303,13 @@ namespace Quiz_Game
                 wrongButton.BackColor= Color.WhiteSmoke;
                 yanlisTimer.Stop();
                 SoruGetir();
+                button5.Select();
             }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
